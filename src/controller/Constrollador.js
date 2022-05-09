c93893851fb0bc2cd764b5605b46dd4f21c3f051
Jackson <jackson_1998@hotmail.com>
2022-05-09 16:00:18 -0300

    Criação da rota detalhes.

--- src/controller/Constrollador.js
+++ src/controller/Constrollador.js
@@ -2,10 +2,20 @@ import { connection } from "../database/connection.js";
 import { itens } from '../model/itens.js';
 
 export const getindex = async (req, res) => {
+    // forma de coletar os dados do banco de dados
     // const listitens = await connection.query('SELECT * FROM itens', {
     //     model: itens
     // })
     const listitens = await itens.findAll();
     console.log(listitens)
     res.render("index.ejs", {listitens}); 
+}
+
+export const getdetalhes = async (req, res) => {
+    const listitens = await itens.findAll();
+    const id = +req.params.id;
+    console.log(+req.params.id);
+    let item = listitens.find((item) => item.id == id);
+    console.log(item);
+    res.render("detalhes.ejs", {item});
 }
--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getindex } from '../controller/Constrollador.js';
+import { getindex, getdetalhes } from '../controller/Constrollador.js';
 
 export const routers = express.Router();
 
 //Rotas
 routers.get("/", getindex);
+routers.get("/detalhes/:id", getdetalhes);
   
 //routers.get("/cadastro", getcadastro);
   
